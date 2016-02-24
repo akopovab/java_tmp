0ba9663223a4558ea65391c2416b1114b1ea55d1
irinaf <irina28@mail.ru>
2016-02-24 18:10:59 +0300

    Класс Point

--- sandbox/src/main/java/ru/stqa/tmp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/tmp/sandbox/MyFirstProgram.java
@@ -13,11 +13,13 @@ public class MyFirstProgram {
        System.out.println("Площадь прямоугольника со сторонами" + r.a+ " и "+ r.b+ "=" + r.area());
 
 
-          Point p1=new Point(1,1);
-          Point p2=new Point(4,4);
+          //Point p1=new Point(1,1);
+          //Point p2=new Point(4,4);
 
-        System.out.println("Расстояние = " + distance(p1,p2));
+        //System.out.println("Расстояние = " + distance(p1,p2));
 
+       Point p=new Point (1,1,4,4);
+      System.out.println("Расстояние = " +p.distance());
 
     }
 
--- sandbox/src/main/java/ru/stqa/tmp/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/tmp/sandbox/Point.java
@@ -10,13 +10,22 @@ package ru.stqa.tmp.sandbox;
 public class Point {
 
     public double x,y;
+    public double x1,y1;
 
-    public Point(double x, double y) {
+    public Point(double x, double y, double x1, double y1) {
 
         this.x=x;
         this.y=y;
+        this.x1=x1;
+        this.y1=y1;
 
     }
 
+    public double distance(){
+
+        double s;
+        s= Math.sqrt((this.x1 - this.x)*2 + (this.y1-this.y)*2);
+        return s;
+    }
 
 }
